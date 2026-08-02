5818fe593fdbe66a2b856120f3120b3c986754ca
Keithin Greene <kgreene012@gmail.com>
2026-08-02 17:02:06 -0400

    connected to MONGODB

--- server/lib/db.js
+++ server/lib/db.js
@@ -3,8 +3,9 @@ import dotenv from "dotenv";
 
 export const connetctDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI);
+        const conn = await mongoose.connect(process.env.MONGODB_URI);
+        console.log(`MongoDB connected: ${conn.connection.host}`);
     } catch (error) {
-        
+        console.log("Error connecting to MongoDB", error)
     }
 }
--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import authRoutes from "../routes/auth.route.js"
 import dotenv from "dotenv"
+import { connetctDB } from "../lib/db.js";
 
 const app = express();
 
@@ -14,4 +15,5 @@ const PORT = process.env.PORT
 
 app.listen(PORT, () =>{
     console.log("Server is running on PORT:" + PORT);
+    connetctDB();
 })
