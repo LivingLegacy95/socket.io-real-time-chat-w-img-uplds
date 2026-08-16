225342ac618856dcf0655794fbde9ed589aca45e
Keithin Greene <kgreene012@gmail.com>
2026-08-16 13:23:52 -0400

    Working on signup page

--- client/chat_app_w_img_uplds/src/views/SignUpPage.jsx
+++ client/chat_app_w_img_uplds/src/views/SignUpPage.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { signup } from "server/controllers/auth.controller";
+import { useAuthHook } from "../hooks/useAuthHook";
 
 const SignUpPage = () => {
-	return <div>SignupPage</div>;
+	const [showPassword, setShowPassword] = useState(false);
+	const [formData, setFormData] = useState({
+		fullName: "",
+		email: "",
+		password: "",
+	});
+	const { signup, isSigningUp } = useAuthHook();
+	const validateForm = () => {};
+	const handleSubmit = (e) => {
+		e.preventDefault();
+	};
+
+	return (
+		<div className="min-h-screen grid lg:grid-cols-2">
+			<div className="flex flex-col justify-center items-center p-6 sm:p-12">
+				<div className="w-full max-w-md space-y-8">
+					<div className="text-center mb-8">
+						<div className=" size-12 rounded-xl bg-primary/10 flex items-center justify-center group-hover:bg-premary/20 transition-colors">
+							<MessageSquare className="size-6 text-primary" />
+						</div>
+						<h1 className="text-2xl font-bold mt-2">Create Account</h1>
+						<p className="text-base-content/60">Get started with your free account</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default SignUpPage;
