9931f3a3ff73fd18afff1983b277f841666b584a
Keithin Greene <kgreene012@gmail.com>
2026-09-09 19:12:55 -0400

    Profile page updated / handleImageUpload

--- client/chat_app_w_img_uplds/src/views/ProfilePage.jsx
+++ client/chat_app_w_img_uplds/src/views/ProfilePage.jsx
@@ -4,7 +4,18 @@ import { Camera, Mail, User } from "lucide-react";
 
 const ProfilePage = () => {
 	const { authUser, isUpdatingProfile, updateProfile } = useAuthHook();
-	const handleImageUpload = async (e) => {};
+	const handleImageUpload = async (e) => {
+		const file = e.target.files[0];
+		if (!file) return;
+
+		const reader = new FileReader();
+		reader.readAsDataURL(file);
+
+		reader.onload = async () => {
+			const base64Image = reader.result;
+			await updateProfile({ profilePic: base64Image });
+		};
+	};
 	return (
 		<div className="h-screen pt-20">
 			<div className="max-w-2xl mx-auto p-4 y-8">
@@ -24,12 +35,12 @@ const ProfilePage = () => {
 							<label
 								htmlFor="avatar-upload"
 								className={`*
-                  absolute bottom-0 right-0
-                  bg-base-content hover:scale-105
-                  p-2 rounded-full cursor-pointer
-                  transition-all duration-200
-                  ${isUpdatingProfile ? "animate-pulse pointer-events-none" : ""}
-                  `}
+                absolute bottom-0 right-0
+                bg-base-content hover:scale-105
+                p-2 rounded-full cursor-pointer
+                transition-all duration-200
+                ${isUpdatingProfile ? "animate-pulse pointer-events-none" : ""}
+                `}
 							>
 								<Camera className="w-5 h-5 text-base-200" />
 								<input
